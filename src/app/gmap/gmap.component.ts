5d2a43a5d62f46f9633b7c2b8ab8e5ab8243e045
JamalGuyo <jamalguyo08@gmail.com>
2019-10-29 22:37:22 +0300

    create a basic map to display

--- src/app/gmap/gmap.component.ts
+++ src/app/gmap/gmap.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HouseService } from '../house.service';
+import { Subscription } from 'rxjs';
+import { HouseModel } from '../house.model';
 
 @Component({
   selector: 'app-gmap',
   templateUrl: './gmap.component.html',
   styleUrls: ['./gmap.component.scss']
 })
-export class GmapComponent implements OnInit {
+export class GmapComponent implements OnInit, OnDestroy {
+  houses: HouseModel[];
+  sub: Subscription;
+  lat = 1.2921;
+  lng = 36.8219;
 
-  constructor() { }
+  constructor(private houseService: HouseService) {}
 
   ngOnInit() {
+    this.sub = this.houseService.getHouses().subscribe({
+      next: houses => (this.houses = houses)
+    });
   }
-
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
 }
