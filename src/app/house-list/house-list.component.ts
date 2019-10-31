c05bf5f7e863f35d33b1507d1f4cac0694761aa1
JamalGuyo <jamalguyo08@gmail.com>
2019-10-31 15:16:06 +0300

    create a border-bottom nav color

--- src/app/house-list/house-list.component.ts
+++ src/app/house-list/house-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HouseService } from '../house.service';
 import { HouseModel } from '../house.model';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-house-list',
@@ -13,7 +14,7 @@ export class HouseListComponent implements OnInit, OnDestroy {
   sub: Subscription;
   searchText = '';
 
-  constructor(private houseService: HouseService) {}
+  constructor(private houseService: HouseService, private router: Router) {}
 
   ngOnInit(): void {
     this.sub = this.houseService.getHouses().subscribe({
@@ -26,5 +27,9 @@ export class HouseListComponent implements OnInit, OnDestroy {
   }
   onSearch(data) {
     this.searchText = data;
+  }
+  openHotelDetails(h): void {
+    this.router.navigate(['/hotel-detail']);
+    this.houseService.selectedHouse = h;
   }
 }
